0caa46922c942bde2f8a5bcdc5cc134ce7a3bcf6
Andres Yrra <andygithub@gmail.com>
2015-08-05 14:25:47 -0700

    rename

--- ZFINALSERVER/src/Server.java
+++ ZFINALSERVER/src/Server.java
@@ -12,15 +12,15 @@ public class Server {
 		ArrayList<Socket> sockets = new ArrayList<Socket>();
 		ArrayList<Double> doubleList = new ArrayList<Double>();
 		ArrayList<Double> doubleList2 = new ArrayList<Double>();
-		double[] numbers = new double[1001];  //we will use 1001 since we will be starting at 1
+		double[] numbers = new double[1000];  //we will use 1001 since we will be starting at 1
 		double[] tempSend = new double[10];   //this array we use for sending out the 10's of the array
 		int servPort = 443;	//port number the server will run on
-		int count = 1;		//start at 1
+		int count = 0;		//start at 1
 		int numOfClients = 5;	//we will be running with 5 clients
 		boolean isLoopDone = false;		//dont change
 		int frames = 0;		//frames will keep track of which frame we're on
-		int low = 1;		//we start at 1
-		int high = 10;		//we send at 10
+		int low = 0;		//we start at 1
+		int high = 9;		//we send at 10
 		
 		loadArray(numbers);	//loads an array with 1000 random integers
 		ServerSocket servSock = new ServerSocket(servPort);		//create the serversocket
@@ -31,7 +31,7 @@ public class Server {
 				count++;
 			}
 			if (count > numOfClients) {
-				for (int i = 1; i <= 100; i++) {	//this will run 100 times so 100 x 10 elements = 1000
+				for (int i = 1; i < 100; i++) {	//this will run 100 times so 100 x 10 elements = 1000
 					grabPartnSet(low,high,numbers,tempSend); //
 					sendStream(frames,tempSend,sockets);	//this will send the stream
 					receiveStream(frames, sockets, doubleList);	//now we will receive 1 integer and store in doubleList
@@ -45,7 +45,7 @@ public class Server {
 				frames = 0;		//reset back to 0
 				low = 0;		//reset low to 0
 				high = 9;		//reset high to 9
-				for (int i = 1; i <= 10; i++) {		//we repeat to do the 100 elements
+				for (int i = 1; i < 10; i++) {		//we repeat to do the 100 elements
 					grab2(low,high,doubleList,tempSend);	
 					sendStream(frames,tempSend,sockets);
 					receiveStream(frames,sockets,doubleList2); //100 elements stored into doubleList2
@@ -99,7 +99,7 @@ public class Server {
 	//creates array of 1000 with 1000 random doubles
 	public static void loadArray(double[] array) {
 		Random random = new Random();
-		for (int i = 1; i <= 1000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			array[i] =  random.nextDouble();
 		}
 	}
