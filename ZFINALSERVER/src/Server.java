37f6d02d68cba6f863875614eeddcea54cd5bd26
Andres Yrra <yrraandres@gmail.com>
2015-09-01 00:03:52 -0700

    Fixed a crashing issue and added variable client-count support.

--- ZFINALSERVER/src/Server.java
+++ ZFINALSERVER/src/Server.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Server {
@@ -14,45 +15,44 @@ public class Server {
 		ArrayList<Double> doubleList2 = new ArrayList<Double>();
 		double[] numbers = new double[1000];  //we will use 1001 since we will be starting at 1
 		double[] tempSend = new double[10];   //this array we use for sending out the 10's of the array
-		int servPort = 443;	//port number the server will run on
-		int count = 0;		//start at 1
-		int numOfClients = 5;	//we will be running with 5 clients
-		boolean isLoopDone = false;		//dont change
-		int frames = 0;		//frames will keep track of which frame we're on
-		int low = 0;		//we start at 1
-		int high = 9;		//we send at 10
+		int servPort = 444;	//port number the server will run on
+		int count = 1;		//start at 1 so the code triggers at numOfClients + 1
+		int numOfClients = 5;	//CHANGE THIS TO HOWEVER MANY CLIENTS YOU WANT TO USE
+		boolean isLoopDone = false;		//dont change, we use this when we are completely finished
+		int frames = 0;		//frames will keep track of which frame we're on    
+		int low = 0;		//low and high used for keeping track of array bounds
+		int high = 9;		
 		
 		loadArray(numbers);	//loads an array with 1000 random integers
 		ServerSocket servSock = new ServerSocket(servPort);		//create the serversocket
-		
 		while (true) {		
 			//accept sockets until we reach designated amount of clients
 			if (sockets.add(servSock.accept())); {	
 				count++;
 			}
 			if (count > numOfClients) {
-				for (int i = 1; i < 100; i++) {	//this will run 100 times so 100 x 10 elements = 1000
-					grabPartnSet(low,high,numbers,tempSend); //
+				for (int i = 0; i < 100; i++) {	//this will run 100 times so 100 x 10 elements = 1000
+					grabPartnSet(low,high,numbers,tempSend); 
 					sendStream(frames,tempSend,sockets);	//this will send the stream
 					receiveStream(frames, sockets, doubleList);	//now we will receive 1 integer and store in doubleList
 					frames++;	//increment by 1
 					low = low + 10;	//set the new low
 					high = high + 10;	//set the new high
-					if (frames > 4)		//when we reach 5 frames we reset back to 0
+					if (frames > numOfClients-1)		//when we reach 5 frames we reset back to 0
 						frames = 0;
 				}
 				//do it again we now have 100 to go through
 				frames = 0;		//reset back to 0
 				low = 0;		//reset low to 0
 				high = 9;		//reset high to 9
-				for (int i = 1; i < 10; i++) {		//we repeat to do the 100 elements
+				for (int i = 0; i < 10; i++) {		//we repeat to do the 100 elements
 					grab2(low,high,doubleList,tempSend);	
 					sendStream(frames,tempSend,sockets);
 					receiveStream(frames,sockets,doubleList2); //100 elements stored into doubleList2
 					frames++;
 					low = low + 10;
 					high = high + 10;
-					if (frames > 4)
+					if (frames > numOfClients-1)
 						frames = 0;
 				}
 				//we now have 10, do it once more
@@ -64,6 +64,8 @@ public class Server {
 			}
 			if (isLoopDone == true) { //answer is printed and we send stop to all the clients
 				System.out.println("The sum of the list is: " + doubleList.get(0));
+				double totalSum = Arrays.stream(numbers).sum();
+				System.out.println("Local server sum (just to verify) is: " + totalSum);
 				sendStop(sockets);
 				break;  //break out and the program ends
 			}
@@ -81,7 +83,7 @@ public class Server {
 	//grabs low and high bounds from an input array, then stores then into an output array
 	public static void grabPartnSet(int lowBound, int highBound, double[] in, double[] out) {
 		int count = 0;
-		for (int i = lowBound; i <= highBound; i++) {
+		for (int i = lowBound; i <= highBound ; i++) {
 			out[count] = in[i];
 			count++;
 		}	
