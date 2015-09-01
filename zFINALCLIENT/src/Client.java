b4b0193b7b5cc87c00c92d84f70293dcc7b790cd
Andres Yrra <yrraandres@gmail.com>
2015-09-01 00:01:37 -0700

    added a sleep to offset some high cpu usage.

--- zFINALCLIENT/src/Client.java
+++ zFINALCLIENT/src/Client.java
@@ -14,7 +14,7 @@ public class Client {
 	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
 		
 		String server = "127.0.0.1";			//input server ip you will be connecting to
-		int servPort = 443;						//port we connect through
+		int servPort = 444;						//port we connect through
 		socket = new Socket(server,servPort);	// create socket for read/write 
 		
 		Thread send = new Thread() {	//here we create the send thread to handle sending 
@@ -22,6 +22,7 @@ public class Client {
 				DataOutputStream dos = null;
 				try {dos = new DataOutputStream(socket.getOutputStream());} catch (IOException e) {}
 				while (true) {
+					try {sleep(15);} catch (InterruptedException e1) {}
 					if (lock == true) {
 						try {dos.writeDouble(arraySum);} catch (IOException e) {}
 						arraySum = 0;
@@ -36,13 +37,15 @@ public class Client {
 				DataInputStream dis = null;
 				try {dis = new DataInputStream(socket.getInputStream());} catch (IOException e) {}
 				while (true) {
+					try {sleep(15);} catch (InterruptedException e1) {}
 					if (lock == false) {
 						for (int i = 0; i < 10; i++) {
-							try {arraySum = arraySum + dis.readDouble();} catch (IOException e) {}
+							try {double temp = dis.readDouble();
+							arraySum = temp + arraySum;
+							} catch (IOException e1) {}
 						}
 						if (arraySum == -10)
 							System.exit(0);
-						System.out.println("Sum sent back : " + arraySum);
 						lock = true;
 					}
 				}
